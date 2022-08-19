8614428f21371412201e6bc66d8d0bc7283fd4ab
christophe Boschmans <cboschmans@protonmail.com>
2022-08-19 13:21:18 +0200

    5.4.2

--- src/main/java/com/polarbookshop/catalogservice/web/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/web/HomeController.java
@@ -1,13 +1,17 @@
 package com.polarbookshop.catalogservice.web;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
-    @GetMapping
+ private final PolarProperties properties;
+ public HomeController(PolarProperties polarProperties){
+     this.properties=polarProperties;
+ }
+    @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to the book catalog!";
+        return properties.getGreeting();
     }
 }
